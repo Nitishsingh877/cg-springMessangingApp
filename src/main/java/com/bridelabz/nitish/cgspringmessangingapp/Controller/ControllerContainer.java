6fd38796e2743cf3ab9bb19354997cffdaa6ab40
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-21 23:37:37 +0530

    [Nitish] added code from get request method and pass query as params

--- src/main/java/com/bridelabz/nitish/cgspringmessangingapp/Controller/ControllerContainer.java
+++ src/main/java/com/bridelabz/nitish/cgspringmessangingapp/Controller/ControllerContainer.java
@@ -1,11 +1,8 @@
 package com.bridelabz.nitish.cgspringmessangingapp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hello")
 public class ControllerContainer {
     //uc01Create a Rest Controller to demonstrate
     //the various HTTP Methods and respond
@@ -14,9 +11,22 @@ public class ControllerContainer {
     //- Use GET Request Method
     //- Use CURL to demonstrate the REST API Call
 
-    @GetMapping
+    @GetMapping("hello")
     public String hello() {
         return " Hello from BridgeLabz";
+    }
+
+    //Make REST Call to show Hello
+    //Mark from BridgeLabz
+    //- Use GET Request Method and pass name as
+    //query parameter
+    //- Use CURL to demonstrate the REST API Call
+    //- curl localhost:8080/hello/query?name=Mark
+    //-w "\n"
+
+    @GetMapping("/hello/query")
+    public String helloWithName(@RequestParam(name = "name",defaultValue = "Guest")String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 
 
